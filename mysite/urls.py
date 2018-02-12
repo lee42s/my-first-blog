8e2515c89ed1b7628eccaacdc6e4cf5d01a4562d
lee42s <lee42s@naver.com>
2018-02-12 12:17:19 +0900

    회원가입,비밀번호변경 기능추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,13 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
 
-from mysite.views import home
+from mysite.views import home,UserRegister,UserRegisterDone
 
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^board/', include('board.urls', namespace='board')),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/register/$', UserRegister.as_view(), name='register'),
+    url(r'^accounts/register/done/$', UserRegisterDone.as_view(), name='register_done'),
     url(r'^admin/', admin.site.urls),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,20 @@
 from blog.models import Post
-
 from board.models import Board
-
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.base import TemplateView
 
 def home(request):
     post = Post.objects.order_by("-created_date")[:5]
     board = Board.objects.order_by("-created_date")[:5]
     return render(request, "home.html", {'post': post, 'board': board})
+
+class UserRegister(CreateView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+class UserRegisterDone(TemplateView):
+    template_name = 'registration/register_done.html'
